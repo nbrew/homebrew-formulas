50a904edc27752306f94f44e14bb99e0a1e83aaf
Nathan Hyde <nhyde@mbayaq.org>
2013-05-16 19:45:24 -0700

    Add haxe and neko formulas.

---
+++ Formula/haxe.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Haxe < Formula
+  homepage 'http://haxe.org/'
+  url 'http://haxe.org/file/haxe-3.0.0-rc2-osx.tar.gz'
+  sha1 '5b631e53114a50fc418fa701c813154f73124667'
+
+  def install
+    bin.install %w(haxe haxedoc haxelib)
+    (share+"haxe").install "std"
+  end
+
+  def caveats; <<-EOS.undent
+    HaXe needs to know how to find its standard library so add this to your
+    shell profile:
+      export HAXE_LIBRARY_PATH="$(brew --prefix)/share/haxe/std"
+    EOS
+  end
+end
---
+++ Formula/neko.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Neko < Formula
+  homepage 'http://nekovm.org/'
+  url 'http://nekovm.org/_media/neko-2.0.0-osx.tar.gz'
+  sha1 '7efe2932e9f3ddb795f758d0958978a4d62d8406'
+
+# cp libneko.dylib /usr/lib
+# cp neko nekoc nekotools nekoml nekoml.std /usr/bin
+# mkdir /usr/lib/neko
+# cp *.ndll /usr/lib/neko
+
+  def install
+    lib.install 'libneko.dylib'
+    prefix.install Dir['*']
+  end
+
+  def caveats; <<-EOS.undent
+    You may need to setup enviroment variable NEKOPATH=#{prefix} and add it to your path.
+    EOS
+  end
+end
